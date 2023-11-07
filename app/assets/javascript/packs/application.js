cf0f157f308cc3fac07c319277fb5ed8ac532434
youngchans <142385527+youngchans@users.noreply.github.com>
2023-11-07 18:50:25 +0900

    NEW

---
+++ app/assets/javascript/packs/application.js
@@ -0,0 +1,4 @@
+require("jquery")
+require("@nathanvda/cocoon")
+require("@rails/ujs").start()
+require('jquery')
---
+++ config/webpack/environment.js
@@ -0,0 +1,12 @@
+const { environment } = require('@rails/webpacker')
+
+// 追記
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+    new webpack.ProvidePlugin({
+        $: 'jquery/src/jquery',
+        jQuery: 'jquery/src/jquery'
+    })
+)
+
+module.exports = environment
