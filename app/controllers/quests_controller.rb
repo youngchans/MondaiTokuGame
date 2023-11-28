f2bc1bc92012bce131ec69c49b210e6284a9a86e
youngchans <142385527+youngchans@users.noreply.github.com>
2023-11-28 16:51:44 +0900

    NEW

--- app/controllers/quests_controller.rb
+++ app/controllers/quests_controller.rb
@@ -1,5 +1,6 @@
 class QuestsController < ApplicationController
   before_action :set_quest, only: %i[ show edit update destroy download]
+  require 'csv'
 
   def index
     if params[:search].present?
@@ -16,6 +17,14 @@ class QuestsController < ApplicationController
   def new
     @quest = Quest.new
     @quest.tasks.build
+    @quests = Quest.where(owner: cookies[:name])
+
+    respond_to do |format|
+      format.html
+      format.csv do |csv|
+        send_quests_csv(@quests)
+      end
+    end
   end
 
   def edit
@@ -90,6 +99,26 @@ class QuestsController < ApplicationController
     def set_quest
       @quest = Quest.find_by(id: params[:id])
     end
+
+  def send_quests_csv(quests)
+    csv_data = CSV.generate do |csv|
+      column_names = %w(単語 意味 本文)
+      csv << column_names
+      quests.each do |quest|
+        column_values = [
+          quest.question,
+          quest.description,
+          if quest.tasks.present?
+            quest.tasks.each do |task|
+              task.similar_word
+            end
+          end,
+        ]
+        csv << column_values
+      end
+    end
+    send_data(csv_data, filename: "投稿一覧.csv")
+  end
 
     def quest_params
       params.require(:quest).permit(:question, :description, tasks_attributes: %i[
